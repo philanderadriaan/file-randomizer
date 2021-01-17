99e4b75537d2e208ae2d12f9bd34ced03efa9e19
philanderadriaan <philanderadriaan@users.noreply.github.com>
2021-01-17 22:25:37 +0800

    Allow user to pick folders when no prop files.

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,102 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.Properties;
 
-public class Main {
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
+public class Main
+{
+
+	/**
+	 * Properties file name.
+	 */
 	private static final String PROP_FILE_NAME = "config.properties";
+
+	/**
+	 * Source folder key in properties file.
+	 */
 	private static final String SRC_KEY = "src";
+
+	/**
+	 * Destination folder key in properties file.
+	 */
 	private static final String DEST_KEY = "dest";
+
+	/**
+	 * Properties object.
+	 */
+	private static final Properties PROP = new Properties();
 
 	/**
 	 * @param args
 	 * @throws IOException
 	 * @throws FileNotFoundException
+	 * @throws UnsupportedLookAndFeelException
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws ClassNotFoundException
 	 */
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException,
+			InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+	{
 		// Log JAVA_HOME.
-		writeLog("JAVA_HOME=" + System.getProperty("java.home"));
+		log("JAVA_HOME=" + System.getProperty("java.home"));
 
-		// Load properties.
-		Properties prop = new Properties();
-		try {
-			prop.load(new FileInputStream(PROP_FILE_NAME));
-		} catch (Exception e) {
-			writeLog("Creating " + PROP_FILE_NAME + ".");
-			prop.setProperty(SRC_KEY, "");
-			prop.setProperty(DEST_KEY, "");
-			prop.store(new FileOutputStream(PROP_FILE_NAME), null);
+		try
+		{
+			// Load properties from file.
+			PROP.load(new FileInputStream(PROP_FILE_NAME));
+		}
+		catch (Exception e)
+		{
+			// Set UI look and feel to current system.
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+			// Choose directories for source and destination.
+			chooseDir(SRC_KEY, "source");
+			chooseDir(DEST_KEY, "destination");
+
+			// Creates new properties file.
+			log("Creating " + PROP_FILE_NAME + ".");
+			PROP.store(new FileOutputStream(PROP_FILE_NAME), null);
 		}
 
 	}
 
-	private static void writeLog(String message) {
+	/**
+	 * Choose directory to add to properties file.
+	 * 
+	 * @param key
+	 */
+	private static void chooseDir(String key, String title)
+	{
 
-		System.out.println("[" + new Date() + "] " + message);
+		// Set file chooser to directories only.
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		chooser.setDialogTitle("Select " + title + " folder");
+
+		// Choose directory.
+		int option = chooser.showOpenDialog(null);
+		if (option == JFileChooser.APPROVE_OPTION)
+		{
+
+			// Save directory to properties object.
+			PROP.setProperty(key, chooser.getSelectedFile().getAbsolutePath());
+		}
+	}
+
+	/**
+	 * Writes to log.
+	 * 
+	 * @param msg
+	 */
+	private static void log(String msg)
+	{
+
+		// Adds timestamp before log message.
+		System.out.println(new Date() + "\t" + msg);
 
 	}
 
