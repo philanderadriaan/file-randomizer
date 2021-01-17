96f746b998693ccaf7b135652491fbe57062db41
philanderadriaan <philanderadriaan@users.noreply.github.com>
2021-01-17 23:27:02 +0800

    Reset destination folder and copy ungrouped object.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Date;
 import java.util.Properties;
+import java.util.Random;
 
 import javax.swing.JFileChooser;
 import javax.swing.UIManager;
@@ -41,19 +45,24 @@ public class Main
 	 * @throws InstantiationException
 	 * @throws ClassNotFoundException
 	 */
-	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException,
-			InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
 	{
+
 		// Log JAVA_HOME.
 		log("JAVA_HOME=" + System.getProperty("java.home"));
 
 		try
 		{
+
 			// Load properties from file.
 			PROP.load(new FileInputStream(PROP_FILE_NAME));
+
 		}
 		catch (Exception e)
 		{
+
+			log(e.getMessage());
+
 			// Set UI look and feel to current system.
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
@@ -64,6 +73,45 @@ public class Main
 			// Creates new properties file.
 			log("Creating " + PROP_FILE_NAME + ".");
 			PROP.store(new FileOutputStream(PROP_FILE_NAME), null);
+
+		}
+
+		// Reset destination directory.
+		File destDir = new File((String) PROP.get(DEST_KEY));
+		log("Resetting " + destDir.getAbsolutePath());
+
+		for (File destObj : destDir.listFiles())
+		{
+
+			destObj.delete();
+
+		}
+
+		destDir.mkdirs();
+
+		// Source directory.
+		File[] srcSubDirs = new File((String) PROP.get(SRC_KEY)).listFiles();
+		File srcSubDir = null;
+
+		while (srcSubDir == null || !srcSubDir.isDirectory())
+		{
+
+			srcSubDir = srcSubDirs[new Random().nextInt(srcSubDirs.length)];
+
+		}
+
+		// Loop through source directory level 2.
+		for (File srcObj : srcSubDir.listFiles())
+		{
+
+			if (srcObj.isFile())
+			{
+
+				log("Copying " + srcObj.getName());
+				Files.copy(srcObj.toPath(), Paths.get(destDir.getAbsolutePath() + '\\' + srcObj.getName()));
+
+			}
+
 		}
 
 	}
@@ -83,12 +131,15 @@ public class Main
 
 		// Choose directory.
 		int option = chooser.showOpenDialog(null);
+
 		if (option == JFileChooser.APPROVE_OPTION)
 		{
 
 			// Save directory to properties object.
 			PROP.setProperty(key, chooser.getSelectedFile().getAbsolutePath());
+
 		}
+
 	}
 
 	/**
